d6825065b271b7e3a567539356291174c65517c7
MLaisney <laisney.melvyn@lyceejulesfil.fr>
2023-09-21 10:26:48 +0200

    Modifie program 4

--- exo3.cpp
+++ exo3.cpp
@@ -3,8 +3,6 @@ using namespace std;
 int main()
 {
     int age_utilisateur(18);
-    cout<<"L'age de l'utilisateur est : ";
-    cout<<age_utilisateur;
-    cout<<"ans."<<endl;
+    cout << "L'age de l'utilisateur est : " << age_utilisateur << "ans." << endl;
     return 0;  
 }
