aed80c61e871a88d171ecd3281eff8b85854de9e
MLaisney <laisney.melvyn@lyceejulesfil.fr>
2023-09-21 10:05:53 +0200

    Add commenter

--- exo2.cpp
+++ exo2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-using namespace std;
+using namespace std; //Ceci est un commentaire sur une ligne
 int main()
+/*Ceci est un commentaire 
+sur plusieurs lignes*/
 {
  cout<<"Hello, World!"<<endl;
  return 0;  
